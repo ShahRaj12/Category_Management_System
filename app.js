5ac9b47ef1e392b341ac5f772b9fca7f36963eb6
ShahRaj12 <78715436+ShahRaj12@users.noreply.github.com>
2025-02-11 23:20:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./src/config/db");
 
-if(process.env.NODE_ENV === 'test'){
-  console.log("in if");
-  dotenv.config({ path: ".env.test" });
-}else{
-  console.log("in else");
-  dotenv.config();
-}
-
+dotenv.config();
 connectDB();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -20,10 +14,7 @@ app.use(cors());
 app.use("/api/auth", require("./src/routes/authRoutes"));
 app.use("/api/categories", require("./src/routes/categoryRoutes"));
 
-
-if (require.main === module) {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
